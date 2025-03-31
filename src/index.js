b10c85178be5861ab9da5b04243521953a535021
Neeth Dseven <labidi.neeth@gmail.com>
2025-03-31 16:50:49 +0200

    voila

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/index.css"
 import { createBrowserRouter, RouterProvider, } from "react-router-dom";
+import About from "./page/About";
 
 
 const router = createBrowserRouter([
-    { path: '/', element: <App /> }
-
+    { path: '/', element: <App /> },
+    {path: '/about', element: <About /> }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
