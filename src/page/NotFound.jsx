c2c7f50c9636e2750c2280a7483b08edb3d1e4f9
ImTheOryo <enory.dhuysser@coda-student.school>
2025-03-31 16:24:04 +0200

    Init branch + NotFound and Salads Page

---
+++ src/page/NotFound.jsx
@@ -0,0 +1,12 @@
+import '../styles/NotFound.css';
+
+    function NotFound () {
+
+        return (
+          <div className="not-found-container">
+              <p>404 Page not found</p>
+          </div>
+        );
+    }
+
+    export default NotFound;
