7bcf248e27a37b739f87dd0d5f71b2307c00419b
ayrazia <thomas.malandain45@gmail.com>
2025-03-31 11:45:31 +0200

    update rooterjsx

--- src/index.js
+++ src/index.js
@@ -1,20 +1,2 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import * as PropTypes from "prop-types";
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-
-
-const router = createBrowserRouter([
-    { path: '/', element: <App /> }
-
-]);
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-RouterProvider.propTypes = {router: PropTypes.any};
-root.render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
