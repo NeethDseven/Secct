1258395dba82914006ea7e97ae389ddb83940c93
ayrazia <thomas.malandain45@gmail.com>
2025-04-01 10:23:19 +0200

    updateRouter

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 import { IoIosArrowDown } from "react-icons/io";
+import { Outlet } from "react-router-dom";
+import Footer from "./footer";
 
 function Header() {
     const [isDropdownOpen, setIsDropdownOpen] = useState(false);
@@ -14,45 +16,49 @@ function Header() {
     };
 
     return (
-        <nav className="nav-list bg-[#E8F0B8] flex items-center">
-            <Link to="/">
-                <img src="/img/lesCrudettesLogo.webp" className="site-title" alt="LesCrudettes" />
-            </Link>
-            <ul className="nav-list flex justify-evenly w-full text-2xl">
-                <li>
-                    <Link to="/">Acceuil</Link>
-                </li>
-                <li>
-                    <Link to="/about">A propos</Link>
-                </li>
-                <li
-                    className="relative"
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
-                >
-                    <button className="focus:outline-none flex items-center">
-                        <IoIosArrowDown className="mr-2" />
-                        Produits
-                    </button>
-                    {isDropdownOpen && (
-                        <ul className="absolute bg-white shadow-md mt-2">
-                            <li>
-                                <Link to="/salade" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Salade et Crudité</Link>
-                            </li>
-                            <li>
-                                <Link to="/vegetable" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Légumes</Link>
-                            </li>
-                            <li>
-                                <Link to="/other" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Herbes et autres</Link>
-                            </li>
-                        </ul>
-                    )}
-                </li>
-                <li>
-                    <Link to="/contact">Contact</Link>
-                </li>
-            </ul>
-        </nav>
+        <div>
+            <nav className="nav-list bg-[#E8F0B8] flex items-center">
+                <Link to="/">
+                    <img src="/img/lesCrudettesLogo.webp" className="site-title" alt="LesCrudettes" />
+                </Link>
+                <ul className="nav-list flex justify-evenly w-full text-2xl">
+                    <li>
+                        <Link to="/">Acceuil</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">A propos</Link>
+                    </li>
+                    <li
+                        className="relative"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                    >
+                        <button className="focus:outline-none flex items-center">
+                            <IoIosArrowDown className="mr-2" />
+                            Produits
+                        </button>
+                        {isDropdownOpen && (
+                            <ul className="absolute bg-white shadow-md mt-2">
+                                <li>
+                                    <Link to="/salade" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Salade et Crudité</Link>
+                                </li>
+                                <li>
+                                    <Link to="/vegetable" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Légumes</Link>
+                                </li>
+                                <li>
+                                    <Link to="/other" className="block px-4 py-2 bg-[#E8F0B8] text-lg min-w-[200px]">Herbes et autres</Link>
+                                </li>
+                            </ul>
+                        )}
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact</Link>
+                    </li>
+                </ul>
+            </nav>
+            <Outlet />
+            <Footer />
+        </div>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './page/App';
-import "./styles/index.css"
-import { createBrowserRouter, RouterProvider, } from "react-router-dom";
-
+import "./styles/index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Header from './component/Header';
+// import About from './page/About';
+// import Salades from './page/Salades';
+// import Vegetables from './page/Vegetables';
+// import Others from './page/Others';
 
 const router = createBrowserRouter([
-    { path: '/', element: <App /> },
-
+    {
+        path: '/',
+        element: <Header />,
+        children: [
+            { path: '/', element: <App /> },
+// { path: '/about', element: <About /> },
+// { path: '/salades', element: <Salades /> },
+// { path: '/vegetables', element: <Vegetables /> },
+// { path: '/others', element: <Others /> },
+        ],
+    },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
